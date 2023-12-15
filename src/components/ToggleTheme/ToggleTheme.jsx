0ddf9a414ef22f26216640fef0ed1815c0bd29d9
Guilherme Junqueira <figueiredoguiu@gmail.com>
2023-12-15 09:37:30 -0300

    Feat: Dark mode using tailwind

---
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -0,0 +1,30 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { useEffect } from "react";
+
+const ToggleTheme = () => {
+  const systemPreference = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  const pageClasses = document.documentElement.classList;
+
+  useEffect(() => {
+    systemPreference && pageClasses.add("dark");
+  });
+  const toggle = () => {
+    pageClasses.toggle("dark");
+  };
+  return (
+    <div className="hidden sm:block">
+      <MoonIcon
+        className="h-8 text-gray-100 cursor-pointer block dark:hidden"
+        onClick={toggle}
+      />
+      <SunIcon
+        className="h-8 text-gray-100 cursor-pointer hidden dark:block"
+        onClick={toggle}
+      />
+    </div>
+  );
+};
+
+export default ToggleTheme;
